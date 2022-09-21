52fd178f093e8a863e4f1d2142835850917c5757
hyperpulsor <rakanfasya@gmail.com>
2022-09-21 20:24:47 +0700

    Feat : Added feature login, logout, and cookies

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from wishlist.views import show_wishlist, return_XML, return_JSON, return_ID
+from wishlist.views import show_wishlist, return_XML, return_JSON, return_ID, register, login_user, logout_user
 
 app_name = 'wishlist'
 
@@ -7,5 +7,8 @@ urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
     path('xml/', return_XML, name='return_XML'),
     path('json/', return_JSON, name='return_JSON'),
-    path('json/<int:id>', return_ID, name='return_ID')
+    path('json/<int:id>', return_ID, name='return_ID'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
